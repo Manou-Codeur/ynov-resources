2ebfcc2823d647bd3e981bbef39a5dbdbb1d6587
manou-codeur <manou-codeur@outlook.com>
2023-10-27 15:24:22 +0000

    tada

--- 2023/m2/docker/udagram/set_env.sh
+++ 2023/m2/docker/udagram/set_env.sh
@@ -2,12 +2,12 @@
 
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=<YOUR_POSTGRES_USER>
-export POSTGRES_PASSWORD=<YOUR_POSTGRES_PASSWORD>
-export POSTGRES_HOST=<YOUR_POSTGRES_HOST>
-export POSTGRES_DB=<YOUR_POSTGRES_DB_NAME>
+export POSTGRES_USER=pg-admin
+export POSTGRES_PASSWORD=root
+export POSTGRES_HOST=db-container
+export POSTGRES_DB=udagram-db
 export AWS_BUCKET=udagram-bucket-1234567 # Please dont change this
 export AWS_REGION=us-east-1 #Please dont change this
 export AWS_PROFILE=default #Please dont change this
 export JWT_SECRET=testing #Please dont change this
-export URL=http://localhost:8100 #Please dont change this
+export URL=http://localhost:8080 #Please dont change this
